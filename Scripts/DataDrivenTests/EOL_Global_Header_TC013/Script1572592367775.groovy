d361e1a445aeaf178e0ceaa771d173baf3573675
tringapps <tringapps@10.1.0.104>
2019-11-01 13:05:30 +0530

    DataDrivenTests added / Newsletter test case

--- Scripts/DataDrivenTests/EOL_Global_Header_TC013/Script1572592367775.groovy
+++ Scripts/DataDrivenTests/EOL_Global_Header_TC013/Script1572592367775.groovy
@@ -12,6 +12,23 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
 
-WebUI.acceptAlert()
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl('https://www.eonline.com/newsletter')
+
+WebUI.click(findTestObject('Object Repository/Newsletter/Page_Newsletter  E Online/span_news_newsletter-landing__radio'))
+
+WebUI.setText(findTestObject('Object Repository/Newsletter/Page_Newsletter  E Online/input_Weekly_newsletter-landing__input'), 
+    MailID)
+
+WebUI.click(findTestObject('Object Repository/Newsletter/Page_Newsletter  E Online/input_Weekly_glossy__btn newsletter-landing__btn'))
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Newsletter/Page_Newsletter  E Online/p_Thank you for subscribing'), 
+    0)
+
+WebUI.closeBrowser()
 
